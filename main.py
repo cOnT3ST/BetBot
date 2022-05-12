9a884f7c35642c2d2dde6d9900db2c7c92b55185
a2kat <a2kat_90@mail.ru>
2022-05-13 01:35:50 +0300

    controller has now exec func

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from controller import Controller
 
 c = Controller()
 
-if "__name__" == '__main__':
-	threading.Thread(target=c.exec_schedule()).start()
+if __name__ == '__main__':
+	threading.Thread(target=c.exec_schedule).start()
 	threading.Thread(target=c.bot.polling(none_stop=True, interval=0)).start()
